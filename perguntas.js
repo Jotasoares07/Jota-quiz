e434543967bbe9115b553e818cb5a85b84950aef
Jotasoares07 <00001098417379sp@al.educacao.sp.gov.br>
2024-10-23 11:27:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual a unica selecao que tem penta?',
+    'A unica e a selecao brasileira'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o unico time brasileiro que tem tres mundiais?',
+    'Sao Paulo FC'
+)
+
+criaCartao(
+    'Filme',
+    'Sobre o que retrata o filme O menino do pijama listrado?',
+    'Sobre um holocausto na Alemanha'
+)
+
+criaCartao(
+    'Filme',
+    'Qual mensagem passa o filme Karate Kid?',
+    'Confianca e coragem para recomecar quantas vezes forem precisas'
+)
